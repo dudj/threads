8f87f6acce86d6898d4a9d83c6a4161fb6a95a5c
dudj <13020078873@163.com>
2020-11-25 16:51:25 +0800

    演示重排序的现象

---
+++ src/jmm/OutOfOrderExecution.java
@@ -0,0 +1,49 @@
+package jmm;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * 演示重排序的现象
+ * “直到达到某个条件才停止”，测试小概率事件
+ */
+public class OutOfOrderExecution {
+    private static int x = 0, y = 0;
+    private static int a = 0, b = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        Thread one = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    countDownLatch.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                a = 1;
+                x = b;
+            }
+        });
+        Thread two = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    countDownLatch.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                b = 1;
+                y = a;
+            }
+        });
+
+        two.start();
+        one.start();
+        countDownLatch.countDown();
+        one.join();
+        two.join();
+
+        System.out.println("x = " + x + ", y = " + y);
+
+    }
+}
