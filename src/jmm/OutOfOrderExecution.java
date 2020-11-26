01d9ea6e4ad86b5ff728e1af99656764d13a1b7d
dudj <13020078873@163.com>
2020-11-26 11:58:02 +0800

    volatile禁止重排序

--- src/jmm/OutOfOrderExecution.java
+++ src/jmm/OutOfOrderExecution.java
@@ -7,8 +7,8 @@ import java.util.concurrent.CountDownLatch;
  * “直到达到某个条件才停止”，测试小概率事件
  */
 public class OutOfOrderExecution {
-    private static int x = 0, y = 0;
-    private static int a = 0, b = 0;
+    private volatile static int x = 0, y = 0;
+    private volatile static int a = 0, b = 0;
 
     public static void main(String[] args) throws InterruptedException {
         int i = 0;
@@ -22,24 +22,24 @@ public class OutOfOrderExecution {
             Thread one = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    a = 1;
                     try {
                         latch.await();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    a = 1;
                     x = b;
                 }
             });
             Thread two = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    b = 1;
                     try {
                         latch.await();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    b = 1;
                     y = a;
                 }
             });
@@ -50,7 +50,7 @@ public class OutOfOrderExecution {
             one.join();
             two.join();
             System.out.println("第"+i+"次，x = " + x + ", y = " + y);
-            if (x == 1 && y == 1) {
+            if (x == 0 && y == 0) {
                 break;
             }
         }
