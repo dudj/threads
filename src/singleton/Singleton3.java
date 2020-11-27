17b0ea8bfba8d53e8f6a568008d5b07e5a752e1a
dudj <13020078873@163.com>
2020-11-27 10:35:41 +0800

    懒汉式（线程不安全）

---
+++ src/singleton/Singleton3.java
@@ -0,0 +1,18 @@
+package singleton;
+
+/**
+ * 懒汉式（线程不安全）
+ */
+public class Singleton3 {
+    private static Singleton3 instance;
+
+    public Singleton3() {
+    }
+
+    public static Singleton3 getInstance() {
+        if(instance == null){
+            instance = new Singleton3();
+        }
+        return instance;
+    }
+}
