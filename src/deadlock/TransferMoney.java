fc411a27a773d28ee3c6a3811d45d1d6a17d83f5
dudj <13020078873@163.com>
2020-11-30 11:08:19 +0800

    用换序来避免转账死锁，demo中使用了hash值，但是一般来说数据库中都有设计主键，主键是唯一的

--- src/deadlock/TransferMoney.java
+++ src/deadlock/TransferMoney.java
@@ -3,11 +3,13 @@ package deadlock;
 /**
  * 银行转账
  * 带有注释的时候 休眠的时候会发生死锁
+ * 用换序来避免转账死锁，demo中使用了hash值，但是一般来说数据库中都有设计主键，主键是唯一的
  */
 public class TransferMoney implements Runnable{
     int flag = 1;
     static Account a = new Account(500);
     static Account b = new Account(500);
+    static Object lock = new Object();
 
     public static void main(String[] args) throws InterruptedException {
         TransferMoney o1 = new TransferMoney();
@@ -38,13 +40,9 @@ public class TransferMoney implements Runnable{
     }
 
     public static void transferMoney(Account from, Account to, int money) {
-        synchronized (from){
-//            try {
-//                Thread.sleep(500);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-            synchronized (to){
+        //java 中每一个对象都有一个hash值 唯一的 但是有可能值一样 几率很小
+        class Helper{
+            public void transfer(Account from, Account to, int money){
                 if (from.balance - money < 0){
                     System.out.println("转账失败，余额不足！");
                     return;
@@ -54,6 +52,32 @@ public class TransferMoney implements Runnable{
                 System.out.println("转账"+money+"元成功");
             }
         }
+        int fromHash = System.identityHashCode(from);
+        int toHash = System.identityHashCode(to);
+        if(fromHash < toHash){
+            synchronized (from){
+                synchronized (to) {
+                    new Helper().transfer(from, to, money);
+                }
+            }
+        }
+        else if(fromHash > toHash){
+            synchronized (to){
+                synchronized (from) {
+                    new Helper().transfer(to, from, money);
+                }
+            }
+        }else {
+            //hash冲突，进行人为操作
+            synchronized (lock){
+                synchronized (to){
+                    synchronized (from) {
+                        new Helper().transfer(to, from, money);
+                    }
+                }
+            }
+        }
+
     }
     static class Account{
         int balance;
